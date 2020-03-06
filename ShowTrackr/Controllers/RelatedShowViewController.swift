09b5ea8916610982af7d5ba5fd6d6d66c910bfb6
Ariunjargal Batjargal <abatjarg@icloud.com>
2020-03-05 23:44:34 -0500

    Added in related show view controller
    (cherry picked from commit 4e8656a6cd536967a91620bbc7c56fbc2ecd595b)

---
+++ ShowTrackr/Controllers/RelatedShowViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RelatedShowViewController.swift
+//  ShowTrackr
+//
+//  Created by abatjarg on 3/5/20.
+//  Copyright © 2020 abatjarg. All rights reserved.
+//
+
+import UIKit
+
+class RelatedShowViewController: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
